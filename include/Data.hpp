367ae48faf14cf85bb01a93be5ef258a49986412
daylyn-n <daylynnguyen33@gmail.com>
2026-03-16 15:23:20 -0700

    okay

---
+++ include/Data.hpp
@@ -0,0 +1,30 @@
+#ifndef DATA_HPP
+#define DATA_HPP
+
+#include "../thirdparty/glm-master/glm/glm.hpp"
+#include "../thirdparty/glm-master/glm/vec3.hpp"
+#include "../thirdparty/glm-master/glm/vec4.hpp"
+#include "../thirdparty/glm-master/glm/mat4x4.hpp"
+#include "../thirdparty/glm-master/glm/ext/matrix_clip_space.hpp"
+#include "../thirdparty/glm-master/glm/gtc/matrix_transform.hpp"
+#include <memory>
+#define GLM_ENABLE_EXPERIMENTAL
+#include "../thirdparty/glm-master/glm/gtx/rotate_vector.hpp"
+
+#include "ShaderProgram.hpp"
+#include "Texture.hpp"
+#include "glad/glad.h"
+#include "VBO.hpp"
+#include "VAO.hpp"
+
+class Data
+{
+    public:
+
+    
+    private:
+
+
+};
+
+#endif
